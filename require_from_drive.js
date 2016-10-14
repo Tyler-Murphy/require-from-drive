7aa402710b7b705da82630c73e13159ec611d6c3
Tyler Murphy <tmurphy@roirevolution.com>
2016-10-14 08:02:11 -0400

    Adds caching for http requests.

--- require_from_drive.js
+++ require_from_drive.js
@@ -4,14 +4,28 @@ const path = require('path')
 
 const address = process.env.REQUIRE_FROM_DRIVE_SERVER_ADDRESS
 const token = process.env.REQUIRE_FROM_DRIVE_SERVER_TOKEN
+const requestCache = new Map()
 
 module.exports = {
   requireFromDrive
 }
 
 function requireFromDrive (filePath) {
-  let splitPath = filePath.split(path.posix.sep)
+  let response = requestWithCache(filePath)
 
+  try {
+    return JSON.parse(response)
+  } catch (error) {
+    return requireFromString(response, filePath)
+  }
+}
+
+function requestWithCache (filePath) {
+  if (requestCache.has(filePath)) {
+    return requestCache.get(filePath)
+  }
+
+  let splitPath = filePath.split(path.posix.sep)
   let response = request('GET', address, {
     qs: {
       splitPath: JSON.stringify(splitPath),
@@ -20,9 +34,6 @@ function requireFromDrive (filePath) {
     cache: 'file'
   }).getBody('utf-8')
 
-  try {
-    return JSON.parse(response)
-  } catch (error) {
-    return requireFromString(response, filePath)
-  }
+  requestCache.set(filePath, response)
+  return response
 }
